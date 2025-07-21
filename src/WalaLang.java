e209d86583ec5f5bb8425a1eec36840f77115a3b
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-21 21:11:46 +0800

    Pre!

--- src/WalaLang.java
+++ src/WalaLang.java
@@ -1,2 +1,28 @@
 public class WalaLang {
+
+    public static void main(String[] args) {
+        typeNgTae tnt = typeNgTae.black;
+
+        switch(tnt)
+        {
+            case puti:
+                System.out.println("Putang ina pare, puti tae mo");
+                break;
+            case brown:
+                System.out.println("Okay normie");
+                break;
+            case black:
+                System.out.println("Pare, pumunta ka na sa hospital. please");
+                break;
+        }
+    }
+
+
+}
+
+enum typeNgTae
+{
+    puti,
+    brown,
+    black
 }
